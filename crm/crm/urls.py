007f9f66e005be3f0417fd96612310fa82327596
loic5015 <loic.hernio@gmail.com>
2022-07-13 15:05:11 +0200

    finalisation du code contracts

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -18,13 +18,17 @@ from django.contrib import admin
 from rest_framework import routers
 from django.urls import path, include
 from management.views import UsersCreateViewset, UsersListViewset
+from event.views import AdminCustomerViewset, AdminContractViewset
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = routers.SimpleRouter()
 router.register('management/create_user', UsersCreateViewset, basename='create-user')
 router.register('management/list_users', UsersListViewset, basename='list-users')
-router.register('customer', UsersListViewset, basename='list-users')
+router.register('customers', AdminCustomerViewset, basename='customers')
+router.register('contracts', AdminContractViewset, basename='contracts')
+
+
 
 
 urlpatterns = [
